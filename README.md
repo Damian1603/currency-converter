6ef23a423c30c82845e5621efeea345ac7f35a95
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-03-02 14:22:22 +0100

    Added js

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 [![currency-converter.png](https://i.postimg.cc/tCLcKrF8/currency-converter.png)](https://postimg.cc/CzsJDsP4)
 
+## Description
+
+Application for converting euro, dollars and British pounds into Polish zlotys.
+
 ## Demo 
 
 https://damian1603.github.io/currency-converter/
--- js/script.js
+++ js/script.js
@@ -17,17 +17,20 @@
         }
     };
 
-    const updateResultText = (amount, result, currency) => {
+    const updateResultText = (result) => {
         const resultElement = document.querySelector(".js-result");
         resultElement.value = result.toFixed(2);
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
+
         const amount = +amountElement.value;
         const currency = currencyElement.value;
+
         const result = calculateResult(amount, currency);
         updateResultText(amount, result, currency)
     };
@@ -36,6 +39,7 @@
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("submit", onFormSubmit);
     };
+
     init();
 };
 
